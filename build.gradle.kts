0c8de5142b231fffcd747d5ba911fe2cfd324c5c
pfournea <peter.fourneau@gmail.com>
2025-12-06 20:36:47 +0100

    add build directory to git ignore file

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,15 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 javafx {
     version = "21.0.2"
     modules = listOf("javafx.controls", "javafx.fxml")
